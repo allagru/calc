d498e1e9449925027670b956e5449664c5bae026
Alla <liliya86@inbox.ru>
2022-07-03 01:54:56 +0300

    создала простой калькулятор

---
+++ js/calc.js
@@ -0,0 +1,24 @@
+function sum() {
+    let x = prompt("Введите первое число", 5);
+    let y = prompt("Введите второе число", 1);
+    let result = +x + +y;
+    alert(`Результат сложения: ${result}`);
+}
+function sub() {
+    let x = prompt("Введите первое число", 5);
+    let y = prompt("Введите второе число", 1);
+    let result = x - y;
+    alert(`Результат вычитания: ${result}`);
+}
+function div() {
+    let x = prompt("Введите первое число", 5);
+    let y = prompt("Введите второе число", 1);
+    let result = x / y;
+    alert(`Результат деления: ${result}`);
+}
+function mul() {
+    let x = prompt("Введите первое число", 5);
+    let y = prompt("Введите второе число", 1);
+    let result = x * y;
+    alert(`Результат умножения: ${result}`);
+}
