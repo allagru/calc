1d402a2befd9979273814bb4ef87ec3680a1333c
Alla <liliya86@inbox.ru>
2022-07-11 00:47:19 +0300

    переделала на input

--- js/calc.js
+++ js/calc.js
@@ -1,24 +1,8 @@
-function sum() {
-    let x = prompt("Введите первое число", 5);
-    let y = prompt("Введите второе число", 1);
-    let result = +x + +y;
-    alert(`Результат сложения: ${result}`);
-}
-function sub() {
-    let x = prompt("Введите первое число", 5);
-    let y = prompt("Введите второе число", 1);
-    let result = x - y;
-    alert(`Результат вычитания: ${result}`);
-}
-function div() {
-    let x = prompt("Введите первое число", 5);
-    let y = prompt("Введите второе число", 1);
-    let result = x / y;
-    alert(`Результат деления: ${result}`);
-}
-function mul() {
-    let x = prompt("Введите первое число", 5);
-    let y = prompt("Введите второе число", 1);
-    let result = x * y;
-    alert(`Результат умножения: ${result}`);
-}
+let first = document.querySelector('.first-number');
+let second = document.querySelector('.second-number');
+let result = document.querySelector('.result');
+
+let sum = () => result.textContent = `Результат сложения: ${Number(first.value) + Number(second.value)}`;
+let sub = () => result.textContent = `Результат вычитания: ${Number(first.value) - Number(second.value)}`;
+let div = () => result.textContent = `Результат деления: ${Number(first.value) / Number(second.value)}`;
+let mul = () => result.textContent = `Результат умножения: ${Number(first.value) * Number(second.value)}`;
