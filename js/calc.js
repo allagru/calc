84a1ab193ef577a22960ba0f0a9fb60778cf6a0d
Alla <liliya86@inbox.ru>
2022-07-19 19:52:41 +0300

    на ноль делить нельзя

--- js/calc.js
+++ js/calc.js
@@ -6,3 +6,21 @@ let sum = () => result.textContent = `Результат сложения: ${Num
 let sub = () => result.textContent = `Результат вычитания: ${Number(first.value) - Number(second.value)}`;
 let div = () => result.textContent = `Результат деления: ${Number(first.value) / Number(second.value)}`;
 let mul = () => result.textContent = `Результат умножения: ${Number(first.value) * Number(second.value)}`;
+
+
+let buttonDiv = document.querySelector(".btn-div");
+buttonDiv.addEventListener("click", clickDiv);
+
+function clickDiv () {
+    if (second.value == 0) {result.textContent = "На ноль делить нельзя!"}
+    else {div()}
+}
+
+let buttonSum = document.querySelector(".btn-sum");
+buttonSum.addEventListener("click", sum);
+
+let buttonSub = document.querySelector(".btn-sub");
+buttonSub.addEventListener("click", sub);
+
+let buttonMul = document.querySelector(".btn-mul");
+buttonMul.addEventListener("click", mul);
